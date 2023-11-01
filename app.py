b0ead8167afc7a1ddd159dd06059b3a67ca0114e
HarshProj <112640334+HarshProj@users.noreply.github.com>
2023-11-01 10:37:54 +0530

    2nd one

--- app.py
+++ app.py
@@ -7,12 +7,20 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db=SQLAlchemy(app)
 
 class User (db.Model):
-    id=db.Column(db.Integer,primary_key)
-    id=db.Column(db.Integer,primary_key)
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(100),primary_key=True)
+    email=db.Column(db.String(100),nullable=False)
+    password=db.Column(db.String(100),nullable=False)
 
 @app.route('/')
 def hell():
     return render_template('index.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 if __name__=="__main__":
     app.run(debug=True,port=3000)
